d3d042964d4cf5daaaaa8b7e7ca8da47d023166e
github-action-benchmark <github@users.noreply.github.com>
2026-09-06 10:05:52 +0000

    add scoop-uv filesystem benchmarks (cargo) benchmark result for 8759a7c3514daab45a573ed568e88282d0a05a6e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788689150928,
+  "lastUpdate": 1788689152668,
   "repoUrl": "https://github.com/ai-screams/scoop-uv",
   "entries": {
     "scoop-uv benchmarks": [
@@ -4549,6 +4549,42 @@ window.BENCHMARK_DATA = {
             "name": "find_executable_in_miss",
             "value": 1112,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hanyul.ryu@hanyul.xyz",
+            "name": "Pignu",
+            "username": "pignuante"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8759a7c3514daab45a573ed568e88282d0a05a6e",
+          "message": "Merge pull request #169 from ai-screams/release-plz-2026-09-06T01-47-21Z\n\nchore: release v0.15.3",
+          "timestamp": "2026-09-06T19:01:20+09:00",
+          "tree_id": "5d2d930aafd1b7945329b653d9e2aa4dc54fc939",
+          "url": "https://github.com/ai-screams/scoop-uv/commit/8759a7c3514daab45a573ed568e88282d0a05a6e"
+        },
+        "date": 1788689152563,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "find_executable_in_hit",
+            "value": 1680,
+            "range": "± 6",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "find_executable_in_miss",
+            "value": 1102,
+            "range": "± 2",
             "unit": "ns/iter"
           }
         ]
